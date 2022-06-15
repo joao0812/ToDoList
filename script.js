218503407b2658dfad006fcf5e0862e536877dc9
joao0812 <joaoesalmeida@hotmail.com>
2022-06-15 17:18:37 -0300

    To do List

---
+++ script.js
@@ -0,0 +1,25 @@
+const add = document.getElementById('add')
+const list = document.getElementById('list')
+const activity = document.getElementById('activity')
+
+var num = 1
+
+
+
+const addActivity = () => {
+    list.innerHTML += `<div id="item${num}" class="itens"> <h4 class="itensText">${activity.value.toUpperCase()}</h4>  <img onclick="deleta(${num})" src="https://cdn-icons-png.flaticon.com/512/1257/1257490.png?w=740" alt="bin" </div>`;
+    list.style.display = 'flex'
+    num++;
+}
+
+
+const deleta = (evento) => {
+    document.getElementById(`item${evento}`).remove()
+    
+    
+    
+}
+
+add.addEventListener('click', addActivity)
+
+
